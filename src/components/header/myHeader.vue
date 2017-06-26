d1c8f458b34c533330887849f3da84a6259b969b
Drama <abandoneddrama@foxmail.com>
2017-06-26 21:08:52 +0800

    fix(loation): webview加载页面失败

--- src/components/header/myHeader.vue
+++ src/components/header/myHeader.vue
@@ -20,7 +20,9 @@
   export default{
     name: 'myHeader',
     data () {
-      return {}
+      return {
+        browser: null // 客户端信息
+      }
     },
     computed: {
       // 动态获取详情页title
@@ -31,11 +33,39 @@
         return this.$route.path
       }
     },
+    created: function () {
+      /*
+      * 智能机浏览器版本信息:
+      *
+      */
+      this.browser = {
+        versions: function () {
+          var u = navigator.userAgent, app = navigator.appVersion
+          return { // 移动终端浏览器版本信息 
+            trident: u.indexOf('Trident') > -1, // IE内核
+            presto: u.indexOf('Presto') > -1, // opera内核
+            webKit: u.indexOf('AppleWebKit') > -1, // 苹果、谷歌内核
+            gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, // 火狐内核
+            mobile: !!u.match(/AppleWebKit.*Mobile.*/) || !!u.match(/AppleWebKit/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // ios终端
+            android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, // android终端或者uc浏览器
+            iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, // 是否为iPhone或者QQHD浏览器
+            iPad: u.indexOf('iPad') > -1, // 是否iPad
+            webApp: u.indexOf('Safari') == -1 // 是否web应该程序，没有头部与底部
+          }
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+      }
+    },
     methods: {
       // 后退
       retreat () {
         if (this.path === '/home') {
-          this.$router.push('/logOut')
+          if (this.browser.versions.ios || this.browser.versions.iPhone || this.browser.versions.iPad) {
+            this.$router.push('/logOut')
+          } else if (this.browser.versions.android) {
+            window.doBack.actionFromJs()
+          }
         } else {
           this.$router.go(-1)
         }
